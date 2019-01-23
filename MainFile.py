a784c1cb17b0c1a448e5f6380c4e4e442a63dd8c
SunMig <1658824587@qq.com>
2019-01-23 15:42:33 +0800

    1.添加约会网站测试功能 2.添加手写数字识别功能

--- MainFile.py
+++ MainFile.py
@@ -9,12 +9,17 @@ def createDataSet():
     return group,labels
 
 groups,labels=createDataSet();
-print("sort result is : "+classify0([0,0],groups,labels,3))
+# print("sort result is : "+classify0([0,0],groups,labels,3))
 #print(tile(labels,(4,1)))
 # sum_group=groups.sum(axis=1)
 # sum_group=sum_group.argsort()
 # print(sum_group)
-print("this is part2's result...")
+# print("this is part2's result...")
 
 datingDataMat,datingLabels=file2matrix('datingTestSet2.txt')
+#约会网站测试分类器
+#datingClassTest()
+
+#手写数字识别的测试，将数字的图像格式转换为分类器使用的向量格式
+handwritingClassTest()
 
--- kNN.py
+++ kNN.py
@@ -1,3 +1,5 @@
+from os import listdir
+
 from numpy import *
 import  operator;
 
@@ -8,15 +10,15 @@ def classify0(inX,dataSet,labels,k):
     sqDistances=sqDiffMat.sum(axis=1) #对矩阵每一行求和，axis=1
     distances=sqDistances**0.5  #对矩阵元素开平方
     sortedDistIndicies=distances.argsort() #矩阵元素升序排序，这里的升序只是把对应的元素的下标拿了出来
-    print(sortedDistIndicies) #可打印观察下
+    # print(sortedDistIndicies) #可打印观察下
     classCount={} #定义字典，来存放标签和标签出现的次数
     for i in range(k):
         voteIlabel=labels[sortedDistIndicies[i]]
         classCount[voteIlabel]=classCount.get(voteIlabel,0)+1 #统计相同标签出现的次数，出现一次就加1
-    print(classCount.items())
+    # print(classCount.items())
     #operator.itemgetter函数，用于获取对象的哪些维的数据，参数为序号
     sortedClassCount=sorted(classCount.items(),key=operator.itemgetter(1),reverse=True) #标签次数降序排序
-    print(sortedClassCount)
+    # print(sortedClassCount)
     return sortedClassCount[0][0]
 
 
@@ -43,3 +45,64 @@ def file2matrix(filename):
         index+=1
 
     return returnMat,classLabelVector
+#归一化数据
+def autoNorm(dataSet):
+    minVals=dataSet.min(0)
+    maxVals=dataSet.max(0)
+    ranges=maxVals-minVals
+    normDataSET=zeros(shape(dataSet))
+    m=dataSet.shape[0]
+    normDataSET=dataSet-tile(minVals,(m,1))
+    normDataSET=normDataSET/tile(ranges,(m,1))
+    return normDataSET,ranges,minVals
+
+
+#约会网站测试
+def datingClassTest():
+    hoRatio=0.10
+    datingDataMat,datingLabels=file2matrix('datingTestSet2.txt')
+    nornMat,ranges,minVals=autoNorm(datingDataMat)
+    m=nornMat.shape[0]
+    numTestVecs=int(m*hoRatio)
+    errorCount=0
+    for i in range (numTestVecs):
+        classifierResult=classify0(nornMat[i,:],nornMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print("the classifier came back with "+str(classifierResult)+" ,the real answer is "+str(datingLabels[i]))
+        if(classifierResult!=datingLabels[i]):errorCount+=1
+    print("the total error rate is "+str(errorCount/float(numTestVecs)))
+
+#转化图像为向量的函数
+def img2vector(filename):
+    returnVect=zeros((1,1024))
+    fr=open(filename)
+    for i in range(32):
+        linStr=fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j]=int(linStr[j])
+    return returnVect
+#手写数字测试函数
+def handwritingClassTest():
+    hwLabels=[]
+    trainingFileList=listdir('trainingDigits')
+    m=len(trainingFileList)
+    trainingMat=zeros((m,1024))
+    for i in range(m):
+        fileNamestr=trainingFileList[i]
+        fileStr=fileNamestr.split('.')[0]
+        classNumStr=int(fileStr.split('_')[0])#记住要把数字转换为int类型
+        hwLabels.append(classNumStr)
+        trainingMat[i,:]=img2vector('trainingDigits/'+fileNamestr)
+    testFileList=listdir('testDigits')
+    errorcount=0
+    n=len(testFileList)
+    for i in range(n):
+        fileNamestr=testFileList[i]
+        fileStr=fileNamestr.split('.')[0]
+        classNumStr=int(fileStr.split('_')[0])
+        vecoterUnderTest=img2vector('testDigits/'+fileNamestr)
+        classifierResult=classify0(vecoterUnderTest,trainingMat,hwLabels,3)
+        print("the calssifier came back with: "+str(classifierResult)+" ,the real answer is : "+str(classNumStr))
+        if(classifierResult!=classNumStr):
+            errorcount+=1
+    print("the total error number is : "+str(errorcount))
+    print("the total error rate is "+str(float(errorcount/n)))
